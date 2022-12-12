2f344d7bc5ac8bb085f9a657ca91ba29a048d4ce
K4ZWRLD <94097110+K4ZWRLD@users.noreply.github.com>
2022-12-12 17:31:21 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   Client: {
     TOKEN: "", // Your bot's TOKEN. Please use this only in VISUAL STUDIO CODE, not in REPLIT. [OPTIONAL]
-    ID: "1050578040540889089" // Your bot's ID. [REQUIRED]
+    ID: "10517349322187285699" // Your bot's ID. [REQUIRED]
   },
 
   Handler: {
-    GUILD_ID: "1039673544877019176", // Your guild/server ID where you want the mails will be created. [REQUIRED]
-    CATEGORY_ID: "1045798639689269299" // Custom category ID where you want the mails will be created on. You can use /setup slash command to create a category without using this variable.
+    GUILD_ID: "1048116316038504498", // Your guild/server ID where you want the mails will be created. [REQUIRED]
+    CATEGORY_ID: "1048116316038504507" // Custom category ID where you want the mails will be created on. You can use /setup slash command to create a category without using this variable.
   },
 
   Modmail: {
     INTERACTION_COMMAND_PERMISSIONS: ['Administrator'], // The permissions for the slash commands that admins/server managers can access only. [REQUIRED]
-    MAIL_MANAGER_ROLES: ['1039686376901320795'], // The roles for the category where moderators can manage mails.
+    MAIL_MANAGER_ROLES: ['1048116316038504502'], // The roles for the category where moderators can manage mails.
     MENTION_MANAGER_ROLES_WHEN_NEW_MAIL_CREATED: false // If this is "true": When a mail created, all the MANAGER_ROLES will be pinged. Else, if "false": It doesn't ping the roles.
   }
 };
